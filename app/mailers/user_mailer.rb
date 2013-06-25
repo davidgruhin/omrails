4871122809b49b4c5a0c543012086c7cc0056790
David Gruhin <dgruhin@gruhin.net>
2013-06-25 15:18:37 -0400

    Add email notification of uploads

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,6 @@ class UserMailer < ActionMailer::Base
     mail(to: 'dgruhin@gruhin.net', subject: 'New Photo upload to Schlachet.it')
   end
   def schlach_email(user)
-    mail(to: 'dgruhin@gmail.com', subject: 'New Photo upload to Schlachet.it')
+    mail(to: 'jeremy.schlachet@chrobinson.com', subject: 'New Photo upload to Schlachet.it')
   end
 end
